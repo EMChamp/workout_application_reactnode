9ac083aee10ec87f1451526fc8f6b4a7553b5f09
EMChamp <lightingthefuse@gmail.com>
2024-07-27 21:11:27 -0700

    add back logging

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,23 @@
+// pages/_app.tsx
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const handleError = (error: any) => {
+      console.error('Global error handler:', error);
+    };
+
+    window.addEventListener('error', handleError);
+    window.addEventListener('unhandledrejection', handleError);
+
+    return () => {
+      window.removeEventListener('error', handleError);
+      window.removeEventListener('unhandledrejection', handleError);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
