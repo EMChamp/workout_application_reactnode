d2b0ce024a065050fba3bf236978ea20b3d0e8f6
EMChamp <lightingthefuse@gmail.com>
2024-07-27 20:59:49 -0700

    docker network changes

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5040;
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI + '/workoutdb', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected'))
